f80fc8ab27f7e6121e52a5f7ec4826f3498f688d
Marcos <marcospegodesousa10@gmail.com>
2023-07-11 07:08:17 -0300

    a

--- src/main/java/com/br/Projetistas/Model/user/User.java
+++ src/main/java/com/br/Projetistas/Model/user/User.java
@@ -95,7 +95,7 @@ try {
 
 
 // Consulta SQL
-String sql = "SELECT DECRYPT(SENHABD, nome_guerra) AS senha FROM PCEMPR WHERE NOME_GUERRA = '" + login + "' ";
+String sql = "SELECT DECRYPT(SENHABD, nome_guerra) AS senha FROM PCEMPR WHERE NOME_GUERRA = '" + login + "' and voipmundoidchamada = 'S' ";
 
 // Executa a consulta
 Statement statement = connection.createStatement();
